8f7a1165758e401c33d764e0b428f3d05a62d20c
anwitabandaru-gif <anwita.bandaru@gmail.com>
2026-01-29 18:04:14 -0800

    config

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-// Supabase Configuration
-// Replace these with your actual Supabase project credentials
 const SUPABASE_URL = 'https://trwuhopggezdtzjtgyyv.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyd3Vob3BnZ2V6ZHR6anRneXl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3MzM3MDUsImV4cCI6MjA4NTMwOTcwNX0.uKUVRRRPpGsXayYg46KQVGmjsDCp5gygHGJPgGSvOG0';
 
